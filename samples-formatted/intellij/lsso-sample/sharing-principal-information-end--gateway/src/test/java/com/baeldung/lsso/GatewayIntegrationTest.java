851082718e580ddacce6776911043b169547e8f6
Ger Roza <rozagerardo@gmail.com>
2021-10-20 14:55:09 -0300

    reformatted using Intellij 2021.2 * This fixes differences in indentations with Eclipse that cannot easily be manipulated with code style configs (maybe related to this issue https://youtrack.jetbrains.com/issue/IDEA-274778)

--- samples-formatted/intellij/lsso-sample/sharing-principal-information-end--gateway/src/test/java/com/baeldung/lsso/GatewayIntegrationTest.java
+++ samples-formatted/intellij/lsso-sample/sharing-principal-information-end--gateway/src/test/java/com/baeldung/lsso/GatewayIntegrationTest.java
@@ -57,7 +57,7 @@ public class GatewayIntegrationTest {
             .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE));
 
         this.webTestClient.mutateWith(mockJwt().jwt(jwt -> jwt.claim("scope", "read write custom")
-            .subject("customSubjectId")))
+                .subject("customSubjectId")))
             .get()
             .uri(BASE_GATEWAY_URL_PATH + "/projects")
             .exchange()
@@ -91,7 +91,7 @@ public class GatewayIntegrationTest {
         String tasksQueryParamsSection = "?projectId=1";
 
         this.webTestClient.mutateWith(mockJwt().jwt(jwt -> jwt.claim("scope", "read write custom")
-            .subject("customSubjectId")))
+                .subject("customSubjectId")))
             .get()
             .uri(BASE_GATEWAY_URL_PATH + "/tasks" + tasksQueryParamsSection)
             .exchange()
@@ -116,7 +116,7 @@ public class GatewayIntegrationTest {
     @Test
     public void givenJwt_whenRequestUnmappedEndpoint_then404NotFound() throws Exception {
         this.webTestClient.mutateWith(mockJwt().jwt(jwt -> jwt.claim("scope", "read write custom")
-            .subject("customSubjectId")))
+                .subject("customSubjectId")))
             .get()
             .uri(BASE_GATEWAY_URL_PATH + "/other")
             .exchange()
